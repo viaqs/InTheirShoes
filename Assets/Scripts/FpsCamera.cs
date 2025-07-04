81b95c8513e881b70e0a6bfe275cc070fa6873b5
*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚ <kalantaitejustina@gmail.com>
2025-07-04 13:22:17 +0300

    basics

---
+++ Assets/Scripts/FpsCamera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsCamera : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform body;
+    public float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        body.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 3f;
+    private CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        var input = new Vector3();
+        input.x = Input.GetAxis("Horizontal");
+        input.z = Input.GetAxis("Vertical");
+
+        var move = (input.x * transform.right + input.z * transform.forward) *speed*Time.deltaTime;
+        controller.Move(move);
+    }
+}
